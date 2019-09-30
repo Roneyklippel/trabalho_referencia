296f96d43afc4405f447ff78e1278c99dff50492
roneyklippel <sala302b@ceines.com.br>
2019-09-30 20:23:21 -0300

    inicio

---
+++ app/src/main/java/es/br/senac/helpdesk/MainActivity.java
@@ -0,0 +1,14 @@
+package es.br.senac.helpdesk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
